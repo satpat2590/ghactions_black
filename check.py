81725c79f146899350902570f203583d9142195f
Satyam <patelsatyam100@gmail.com>
2023-09-18 14:20:39 -0400

    finished

--- check.py
+++ check.py
@@ -1,13 +1,14 @@
-import os 
+import os
 
 
 def walk_the_directory():
-    file_list = [(x, y, z) for x, y, z in os.walk('.')]
+    file_list = [(x, y, z) for x, y, z in os.walk(".")]
     for idx, files in enumerate(file_list):
         if idx < 10:
             for file in files[2]:
                 print(os.path.join(files[0], file))
         else:
-            break 
+            break
+
 
 walk_the_directory()
--- more_py/tokenizer.py
+++ more_py/tokenizer.py
@@ -1,6 +1,6 @@
 import tiktoken
 
-tokenizer = tiktoken.get_encoding('cl100k_base')
+tokenizer = tiktoken.get_encoding("cl100k_base")
 
 long_text = """
     Gerald went to the store to buy ice cream and then feed his kids the ice cream but by the time he came home, the ice cream melted 
@@ -14,4 +14,5 @@ tokens = tokenizer.encode(long_text)
 
 single_tokens_from_id = [tokenizer.decode_single_token_bytes(x) for x in tokens]
 
-print(tokens); print(single_tokens_from_id)
+print(tokens)
+print(single_tokens_from_id)
