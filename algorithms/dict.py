1b79fffecef9f07757b906c8fd2152321c044d47
Satyam <patelsatyam100@gmail.com>
2023-09-19 14:20:08 -0400

    black code formatter changed files

--- algorithms/dict.py
+++ algorithms/dict.py
@@ -1,11 +1,11 @@
-class Dict():
+class Dict:
     def __init__(self):
         pass
 
     def dictionary_example(self):
         mapping = {}
         # Create list of Students and their Scores on the recent exam
-        students = ['Jeremy', 'Vanessa', 'Jeffrey', 'Jahseh', 'Satyam']
+        students = ["Jeremy", "Vanessa", "Jeffrey", "Jahseh", "Satyam"]
         scores = [95, 90, 45, 76, 85, 81]
         # Loop through the array and embed them within the dictionary
         for x in range(len(students)):
@@ -15,14 +15,14 @@ class Dict():
             print(x)
 
     def sum_of_values(self, dict=None):
-        sum = 0 
+        sum = 0
         for key, val in dict.items():
             if not isinstance(val, int):
                 print(f"{key}'s value is not a valid integer. Skipping...")
             else:
                 sum += val
 
-        print(f"Final sum: {sum}")  
+        print(f"Final sum: {sum}")
 
     def sort_keys(self, dict):
         keys = list(dict.keys())
@@ -30,12 +30,10 @@ class Dict():
         print("Original", dict.keys())
         print("Sorted", keys)
 
+
 dict = Dict()
 
-dictionary = {"hello": 4, "world": 'Jacobi', "I": 1, "am": 2, "Martha": 3}
+dictionary = {"hello": 4, "world": "Jacobi", "I": 1, "am": 2, "Martha": 3}
 dict.sum_of_values(dictionary)
 dict.sort_keys(dictionary)
 
-tupley = ("maple")
-
-print(type(tupley))
--- algorithms/loops.py
+++ algorithms/loops.py
@@ -1,4 +1,4 @@
-class Loops():
+class Loops:
     def __init__(self):
         pass
 
@@ -6,17 +6,18 @@ class Loops():
         # Loop through from 2 to x - 1
         for i in range(2, (x // 2) + 1):
             if self.is_prime(i) and self.is_prime(x - i):
-                print(f"{i} + {x - i} = {x} ; Where both numbers in the summation are primes.\n")
-
+                print(
+                    f"{i} + {x - i} = {x} ; Where both numbers in the summation are primes.\n"
+                )
 
     def is_prime(self, x):
         # Loop through from 2 to x - 1. If x is divisible by any number in that range, return False (not prime)
         for i in range(2, (x // 2) + 1):
             if x % i == 0:
                 return False
-        # None of the numbers in the range are factors. x is therefore prime. 
+        # None of the numbers in the range are factors. x is therefore prime.
         return True
-    
+
 
 loop = Loops()
 
